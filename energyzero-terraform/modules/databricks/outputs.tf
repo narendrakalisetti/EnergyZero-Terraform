09ca5d31dcd4a2229ca92f328957e81a32d7e67d
Narendra kalisetti <138960270+narendrakalisetti@users.noreply.github.com>
2026-03-29 18:05:09 +0100

    Delete energyzero-terraform directory

--- energyzero-terraform/modules/databricks/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "databricks_workspace_id"  { value = azurerm_databricks_workspace.main.id }
-output "databricks_workspace_url" { value = azurerm_databricks_workspace.main.workspace_url }
-output "databricks_principal_id"  { value = azurerm_databricks_workspace.main.storage_account_identity[0].principal_id }
