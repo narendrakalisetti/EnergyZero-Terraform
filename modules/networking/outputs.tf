4b81fc19b9d8406af7d63131b1be1feb8827fb2c
Narendra kalisetti <138960270+narendrakalisetti@users.noreply.github.com>
2026-04-13 23:10:46 +0100

    Delete modules directory

--- modules/networking/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "vnet_id"                      { value = azurerm_virtual_network.main.id }
-output "private_endpoint_subnet_id"   { value = azurerm_subnet.private_endpoints.id }
-output "databricks_public_subnet_id"  { value = azurerm_subnet.databricks_public.id }
-output "databricks_private_subnet_id" { value = azurerm_subnet.databricks_private.id }
